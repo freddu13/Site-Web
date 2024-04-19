e1308935adb673bc50af4ef26a8a775751489108
Tom Graciet <tom.graciet@orange.fr>
2024-04-19 15:58:10 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const projects = [
-        { title: "Uno", lang: "Python", description: "projet tldraw", link: "https://github.com/freddu13/tldraw-project" },
-        { title: "Dos", lang: "Python, Pandas", description: "code d'un jeu snake", link: "https://github.com/freddu13/snake.tmp" },
-        { title: "Tres", lang: "Python", description: "hackathon 1", link: "https://github.com/freddu13/pe-hackathon" }
+        { title: "Uno", lang: "Python", description: "projet tldraw miam", link: "https://github.com/freddu13/tldraw-project" },
+        { title: "Dos", lang: "Python, Pandas", description: "code d'un jeu snake oicoubeh", link: "https://github.com/freddu13/snake.tmp" },
+        { title: "Tres", lang: "Python", description: "hackathon 1 pagnan", link: "https://github.com/freddu13/pe-hackathon" }
     ];
 
     // Fonction pour créer une ligne de tableau HTML pour chaque projet
